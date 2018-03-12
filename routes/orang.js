bc18c518c4ddec16582016f08f277c5fb314c1f1
ppsari <holie.nait@gmail.com>
2018-03-12 14:40:59 +0700

    routes & controllers orang

--- routes/orang.js
+++ routes/orang.js
@@ -1,5 +1,11 @@
 const router = require('express').Router()
 
-router.get('/', (req,res)=> res.send('alive'))
+const orangCtrl = require('../controllers/orangCtrl')
+
+router.get('/', orangCtrl.getAll)
+router.get('/:id', orangCtrl.getOne)
+router.post('/', orangCtrl.create)
+router.put('/:id', orangCtrl.update)
+router.delete('/:id', orangCtrl.remove)
 
 module.exports = router
