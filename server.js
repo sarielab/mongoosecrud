2c2f06f04ee207ec59ccb1174c5939ef622d9c83
ppsari <holie.nait@gmail.com>
2018-03-12 14:46:17 +0700

    routes & controllers buku

--- server.js
+++ server.js
@@ -6,10 +6,12 @@ const mongoose = require('mongoose')
 const mongo_url = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ds127132.mlab.com:27132/purwadb`
 
 const orang = require('./routes/orang')
+const buku = require('./routes/buku')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
 app.use('/orang', orang)
+app.use('/buku', buku)
 
 mongoose.connect(mongo_url, function(err, res){
   if(err) console.log(err)
