142a875ee44ad206189b48be82c93326e9154078
ppsari <holie.nait@gmail.com>
2018-03-12 14:31:12 +0700

    create models

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 require('dotenv').config()
 const app = require('express')()
+const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 
 const mongo_url = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ds127132.mlab.com:27132/purwadb`
+
+const orang = require('./routes/orang')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+app.use('/orang', orang)
 
 mongoose.connect(mongo_url, function(err, res){
   if(err) console.log(err)
