1e2f2b4583e321c4bb462973139bac0028f72c82
ppsari <holie.nait@gmail.com>
2018-03-12 17:19:26 +0700

    add _buku_favorit

--- models/orang.js
+++ models/orang.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
 
 let orangSchema = mongoose.Schema({
-  nama: String
+  nama: String,
+  _buku_favorit: {type: mongoose.Schema.ObjectId, ref:'tbl_buku'}
 })
 
 let Orang = mongoose.model('tbl_orang', orangSchema)
