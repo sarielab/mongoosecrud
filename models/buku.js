e17e1ffe58a744de81ee7a67a1ef1b0c15261834
ppsari <holie.nait@gmail.com>
2018-03-12 15:01:30 +0700

    add genre, koreksi ref _pengarang

--- models/buku.js
+++ models/buku.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose')
 
 let bukuSchema = mongoose.Schema({
   judul: String,
-  _pengarang: {type: mongoose.Schema.ObjectId, ref: 'Orang'}
+  _pengarang: {type: mongoose.Schema.ObjectId, ref: 'tbl_orang'},
+  genre : [{type: mongoose.Schema.ObjectId, ref: 'tbl_genre'}]
 })
 
 let Buku = mongoose.model('tbl_buku', bukuSchema)
